b2c9ce2a01c40f986ead67438243e31ebc944457
amichno <95045327+amichno@users.noreply.github.com>
2026-08-07 23:02:16 +0200

    feature/addLAnguageContext

---
+++ src/i18n/translations.js
@@ -0,0 +1,24 @@
+export const translations = {
+  en: {
+    ui: {
+      home: {},
+      about: {},
+      recipes: {},
+    },
+  },
+
+  pl: {
+    ui: {
+      home: {},
+      about: {},
+      recipes: {},
+    },
+  },
+};
+
+export const LOCALES = Object.keys(translations);
+
+export const LOCALE_LABELS = {
+  en: 'English',
+  pl: 'Polski',
+};
