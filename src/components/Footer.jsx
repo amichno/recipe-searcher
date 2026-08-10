2170d882b881c9911ed5ed3a3c177b5d7cf41835
amichno <95045327+amichno@users.noreply.github.com>
2026-08-10 23:37:17 +0200

    addIconsToFooter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,16 @@
+import InstaIcon from '../components/icons/icon-instagram.svg';
+import TweeterIcon from '../components/icons/icon-bluesky.svg';
+import TikTokIcon from '../components/icons/icon-tiktok.svg';
+
 export function Footer() {
   return (
-    <footer className="flex items-center justify-between px-10 py-6 border-t border-stone-200 text-xs text-stone-500">
+    <footer className='flex items-center justify-between px-10 py-6 border-t border-stone-200 text-xs text-stone-500'>
       <span>Made with care and fresh vegetables</span>
-      <span>Zdrowe Przepisy</span>
+      <span className='inline-flex items-center gap-2'>
+        <img src={InstaIcon} />
+        <img src={TweeterIcon} />
+        <img src={TikTokIcon} />
+      </span>
     </footer>
   );
 }
