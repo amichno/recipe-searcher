1a3a3b839b9e2ad892a57c9be6340e4123597365
amichno <95045327+amichno@users.noreply.github.com>
2026-08-11 23:38:52 +0200

    style/add emoji, change colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ import TikTokIcon from '../components/icons/icon-tiktok.svg';
 export function Footer() {
   return (
     <footer className='flex items-center justify-between px-10 py-6 border-t border-stone-200 text-xs text-stone-500'>
-      <span>Made with care and fresh vegetables</span>
-      <span className='inline-flex items-center gap-2'>
+      <span>Made with ❤️ and 🥑</span>
+      <span className='inline-flex items-center gap-4'>
         <img src={InstaIcon} />
         <img src={TweeterIcon} />
         <img src={TikTokIcon} />
